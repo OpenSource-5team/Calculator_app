86f3750e04b7419b3e09d1edc4fe9db6abb01af5
Hoyoung Jung <pon07060@gmail.com>
2021-05-28 21:32:17 +0900

    기본적인 계산기 xml , java 올립니다.

--- app/src/main/java/com/N5_team/Calculator/hoyeongActivity.java
+++ app/src/main/java/com/N5_team/Calculator/hoyeongActivity.java
@@ -1,45 +1,69 @@
 package com.N5_team.Calculator;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 
 public class hoyeongActivity extends AppCompatActivity {
 
-    Button btn_1,btn_2;
+
+    Button clear_button,result_button;
+    EditText edit1,edit2,edit3;
+    ImageView imagebutton1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hoyeong);
 
-        btn_1 = (Button)findViewById(R.id.btn_1);
-        btn_2 = (Button)findViewById(R.id.btn_2);
+        clear_button = (Button)findViewById(R.id.clear_button);
+        result_button = (Button)findViewById(R.id.result_button);
+
+        edit1 = (EditText)findViewById(R.id.edit1);
+        edit2 = (EditText)findViewById(R.id.edit2);
+        edit3 = (EditText)findViewById(R.id.edit3);
 
 
-        btn_1.setOnClickListener(new View.OnClickListener() {
+
+        clear_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                hoyeongfragmenta activity_hoyeong1 = new hoyeongfragmenta();
-                transaction.replace(R.id.frame,activity_hoyeong1);
-                transaction.commit();
-
+                edit1.setText("");
+                edit2.setText("");
             }
         });
 
-        btn_2.setOnClickListener(new View.OnClickListener() {
+        imagebutton1 = (ImageView) findViewById(R.id.imagebutton1);
+
+        imagebutton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                hoyeongfragmentb activity_hoyeong2 = new hoyeongfragmentb();
-                transaction.replace(R.id.frame,activity_hoyeong2);
-                transaction.commit();
-
+                Toast.makeText(getApplicationContext(),"오픈소스5팀홧팅",Toast.LENGTH_SHORT).show();
             }
         });
+
+
+    }
+
+    public void onDivButtonClicked(View view) {
+        String tmpEt1 = edit1.getText().toString();
+        String tmpEt2 = edit2.getText().toString();
+
+        int int_edit1 = Integer.parseInt(tmpEt1);
+        int int_edit2 = Integer.parseInt(tmpEt2);
+
+        int result = int_edit1 - (int_edit1 / int_edit2) ;
+        String str_result = Integer.toString(result);
+        edit3.setText(str_result);
     }
 }
